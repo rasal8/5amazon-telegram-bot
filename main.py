00941b05e2575e00136a923c717600e155de0581
rasal8 <mobileaccessories122@gmail.com>
2026-05-16 15:25:22 +0530

    Replace bot token and chat ID with real values
    Updated the bot token and chat ID with actual values.

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import random
 from bs4 import BeautifulSoup
 from telegram import Bot
 
-BOT_TOKEN = "YOUR_BOT_TOKEN"
-CHAT_ID = "YOUR_CHAT_ID"
+BOT_TOKEN = "8843677021:AAEFr8bnX6szlIcvXV4gFs5OCt9CceBx1fQ"
+CHAT_ID = "8843677021"
 
 bot = Bot(token=BOT_TOKEN)
 
