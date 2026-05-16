fe24fe038f75092fe96eda1c94a3fcf05111658f
rasal8 <mobileaccessories122@gmail.com>
2026-05-16 17:02:36 +0530

    Initialize Telegram bot with token and chat ID
    Added Telegram bot setup with token and chat ID.

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+import asyncio
+from telegram import Bot
+
+BOT_TOKEN = "8843677021:AAEFr8bnX6szlIcvXV4gFs5OCt9CceBx1fQ"
+CHAT_ID = "-1003615762835"
+
 async def send_products():
 
     bot = Bot(token=BOT_TOKEN)
