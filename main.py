b4108aaf554182cbe355eb8a08abc33e42f45a0b
rasal8 <mobileaccessories122@gmail.com>
2026-05-16 16:14:23 +0530

    Fix syntax error in categories dictionary

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from telegram import Bot
 BOT_TOKEN = "8843677021:AAEFr8bnX6szlIcvXV4gFs5OCt9CceBx1fQ"
 CHAT_ID = "-1003615762835"
 
-categories = {
     categories = {
     "🏠 Home Decor": "korean home decor aesthetic room decor lamp beige",
     
